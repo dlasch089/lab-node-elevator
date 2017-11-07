1135ae938d26084ee7274ff495afe96cc0c660dc
Dominik Lasch <dl@exutec.de>
2017-11-07 09:06:07 +0100

    iteration 2

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,41 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setinterval(() => this.update(), 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  update() {
+    this.log();
+  }
+
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+    }
+  }
+
+  floorDown() {
+    if (this.floor < 1) {
+      this.floor--;
+    }
+  }
+
+  call() {}
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
